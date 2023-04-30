586030cf9d119f0b6ab3e6aaa827c390e94fbd42
Ewa Olczak <ewa.olczak@gmail.com>
2023-04-30 21:18:53 +0200

    Prepare app to public on Replit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
-// import db
-const db = require('./db.js');
+const path = require('path');
 
 const app = express();
 
@@ -14,12 +12,18 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use((req, res) => {
