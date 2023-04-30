f4e3b506a3b0171d0d7f213dc41e7d58120fe48e
Ewa Olczak <ewa.olczak@gmail.com>
2023-04-30 21:02:53 +0200

    Modify ticket ordering

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -15,8 +15,18 @@ router.route('/seats').post((req, res) => {
   const { day, seat, client, email } = req.body;
   const id = uuid();
   const newSeat = { id: id, day, seat, client, email };
-  db.seats.push(newSeat);
-  res.json({ message: 'ok!' });
+  if (
+    db.seats.some(
+      (seatCheck) =>
+        seatCheck.day === newSeat.day && seatCheck.seat === newSeat.seat
+    )
+  ) {
+    res.json({ message: 'The slot is already taken...' });
+    res.status(409).json({ message: 'The slot is already taken...' });
+  } else {
+    db.seats.push(newSeat);
+    res.json({ message: 'ok!' });
+  }
 });
 
 router.route('/seats/:id').put(
