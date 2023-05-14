bd75a37d13a6e1b3ef96a05a6d999b1ce1958762
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-14 18:29:41 +0200

    fix server.js error

--- server.js
+++ server.js
@@ -36,15 +36,15 @@ app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-// const server = app.listen(process.env.PORT || 8000, () => {
-//   console.log("Server is running on port: 8000");
-// });
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running on port: 8000");
+});
 
-// const io = socket(server);
+const io = socket(server);
 
-// io.on("connection", (socket) => {
-//   console.log("Client connected with ID: " + socket.id);
-// });
+io.on("connection", (socket) => {
+  console.log("Client connected with ID: " + socket.id);
+});
 
 // connects our backend code with the database
 mongoose.connect(uri, {
