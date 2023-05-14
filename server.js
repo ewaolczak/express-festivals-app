39a8cb381d42e6dfe6071948bc97d6754eda3404
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-14 17:16:03 +0200

    Add cloud server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const socket = require('socket.io');
 const mongoose = require('mongoose');
+const uri =
+  'mongodb+srv://ewaolczak:g3msCFduZ5wFFOWU@ewa-olczak.nryeduz.mongodb.net/NewWaveDB';
 
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -21,7 +24,7 @@ app.use('/api', seatsRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use((req, res, next) => {
-  req.db = db;
+  req.io = io;
   next();
 });
 
@@ -33,13 +36,23 @@ app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
+// const server = app.listen(process.env.PORT || 8000, () => {
+//   console.log("Server is running on port: 8000");
+// });
+
+// const io = socket(server);
+
+// io.on("connection", (socket) => {
+//   console.log("Client connected with ID: " + socket.id);
+// });
+
 // connects our backend code with the database
-mongoose.connect('/mongodb://localhost:27017/NewWaveDB', {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 db.once('open', () => {
   console.log('Connected to the database');
