6d152f56bbb17c5a62cc305ff98f626c46be1bb5
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-14 15:00:11 +0200

    separate seat controller

---
+++ controllers/seats.controller.js
@@ -0,0 +1,63 @@
+const Seat = require('../models/seat.model');
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Seat.find());
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const singleSeat = await Seat.findById(req.params.id);
+    if (!singleSeat) res.status(404).json({ message: 'Not found' });
+    else res.json(singleSeat);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.post = async (req, res) => {
+  try {
+    const { day, seat, client, email } = req.body;
+    const newSeat = new Seat({
+      day: day,
+      seat: seat,
+      client: client,
+      email: email
+    });
+    await newSeat.save();
+    res.json({ message: 'ok!' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.put = async (req, res) => {
+  const { day, seat, client, email } = req.body;
+  try {
+    const singleSeat = await Seat.findById(req.params.id);
+    if (singleSeat) {
+      await Seat.updateOne(
+        { _id: req.params.id },
+        { $set: { day: day, seat: seat, client: client, email: email } }
+      );
+      res.json({ message: 'OK!' });
+    } else res.status(404).json({ message: 'Not found' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const singleSeat = await Seat.findById(req.params.id);
+    if (singleSeat) {
+      await Seat.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK!' });
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,67 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Seat = require('../models/seat.model');
+const SeatController = require('../controllers/seats.controller');
 
-router.get('/seats', async (req, res) => {
-  try {
-    res.json(await Seat.find());
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
-
-router.get('/seats/:id', async (req, res) => {
-  try {
-    const singleSeat = await Seat.findById(req.params.id);
-    if (!singleSeat) res.status(404).json({ message: 'Not found' });
-    else res.json(singleSeat);
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
-
-router.post('/seats', async (req, res) => {
-  try {
-    const { day, seat, client, email } = req.body;
-    const newSeat = new Seat({
-      day: day,
-      seat: seat,
-      client: client,
-      email: email
-    });
-    await newSeat.save();
-    res.json({ message: 'ok!' });
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
-
-router.put('/seats/:id', async (req, res) => {
-  const { day, seat, client, email } = req.body;
-  try {
-    const singleSeat = await Seat.findById(req.params.id);
-    if (singleSeat) {
-      await Seat.updateOne(
-        { _id: req.params.id },
-        { $set: { day: day, seat: seat, client: client, email: email } }
-      );
-      res.json({ message: 'OK!' });
-    } else res.status(404).json({ message: 'Not found' });
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
-
-router.delete('/seats/:id', async (req, res) => {
-  try {
-    const singleSeat = await Seat.findById(req.params.id);
-    if (singleSeat) {
-      await Seat.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK!' });
-    } else res.status(404).json({ message: 'Not found...' });
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.get('/seats', SeatController.getAll);
+router.get('/seats/:id', SeatController.getById);
+router.post('/seats', SeatController.post);
+router.put('/seats/:id', SeatController.put);
+router.delete('/seats/:id', SeatController.delete);
 
 module.exports = router;
