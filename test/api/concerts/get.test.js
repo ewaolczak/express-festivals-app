1deb065f2ccaf27be541e149118db56d8f44e630
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-21 13:06:49 +0200

    Fix chai error

--- test/api/concerts/get.test.js
+++ test/api/concerts/get.test.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 const server = require('../../../server');
 const Concert = require('../../../models/concert.model');
 
-use.chai(chaiHttp);
+chai.use(chaiHttp);
 
 const expect = chai.expect;
 const request = chai.request;
